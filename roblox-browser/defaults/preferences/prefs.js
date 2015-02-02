783be87594cee0aa89fac7504f7a0cbc764d1e10
Jonathan Alfonso <alfonsojon1997@gmail.com>
2015-02-02 01:19:38 -0600

    Update prefs.js

--- roblox-browser/defaults/preferences/prefs.js
+++ roblox-browser/defaults/preferences/prefs.js
@@ -1,2 +1,10 @@
 pref("toolkit.defaultChromeURI", "chrome://roblox-browser/content/roblox-browser.xul");
+pref("toolkit.defaultChromeURI", "chrome://roblox-browser/content/roblox-browser.xul");
 GeckoPreferences.Default["extensions.blocklist.enabled"] = false;
+
+/* debugging prefs */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
